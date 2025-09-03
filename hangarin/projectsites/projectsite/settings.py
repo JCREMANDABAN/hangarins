9850a1373346fc994cd1762461d4ed3129ce4d58
John Christopher Remandaban <202380127@psu.palawan.edu.ph>
2025-09-03 21:09:36 +0800

    modify allowed host

--- hangarin/projectsites/projectsite/settings.py
+++ hangarin/projectsites/projectsite/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['killeritot.pythonanywhere.com', '127.0.0.1']
 
 
-# Application definition app
+# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
